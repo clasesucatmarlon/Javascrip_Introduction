69be04b1847daad24dcf1f04a7f60cf242fb854b
Marlon Aurelio García Morales <clasesucatmarlon@gmail.com>
2020-09-16 14:58:26 +0000

    Print a sentence

--- Exercise_001.js
+++ Exercise_001.js
@@ -1,5 +1,5 @@
 /*
-Escribe un programa que le pida al usuario ingresar una frase y la imprima en la consola.
+Escribir un programa que le pida al usuario ingresar una frase y la imprima en la consola.
 
 Recuerda que para pedirle al usuario que escriba una frase debes utilizar el método prompt() y para escribir en la consola debes utilizar el método console.log().
 */
