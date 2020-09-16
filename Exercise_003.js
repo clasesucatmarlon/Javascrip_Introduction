e7b46404cbcac2981be308329a01e56a75a651a5
Marlon Aurelio García Morales <clasesucatmarlon@gmail.com>
2020-09-16 14:56:51 +0000

    Addition of two numbers

--- Exercise_003.js
+++ Exercise_003.js
@@ -1,5 +1,5 @@
 /*
-Escribe un programa que le pida al usuario ingresar un número, luego le pide un segundo número, e imprima en la consola la suma de los dos números que ingresó el usuario.
+Escribir un programa que le pida al usuario ingresar un número, luego le pide un segundo número, e imprima en la consola la suma de los dos números que ingresó el usuario.
 */
 let valorA = prompt('Ingrese el primer número ')
 let valorB = prompt('Ingrese el segundo número ')
