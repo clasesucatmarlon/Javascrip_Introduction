bb246eccc4b21a5d4b9cd3b174ea2bec563af94d
Marlon Aurelio García Morales <clasesucatmarlon@gmail.com>
2020-09-16 14:54:22 +0000

    Calculate the body mass index (BMI)

--- Exercise_005.js
+++ Exercise_005.js
@@ -4,7 +4,7 @@ El índice de masa corporal (IMC), o BMI por sus siglas en inglés, es un valor 
 El BMI se calcula con la siguiente formula:
 
 peso / altura^2
-Escribe un programa que le pida al usuario su peso y su altura para calcular su BMI e imprima la frase "Tu BMI es X".
+Escribir un programa que le pida al usuario su peso y su altura para calcular su BMI e imprima la frase "Tu BMI es X".
 
 Por ejemplo, si el usuario ingresa 65 de peso y 1.8 de altura el programa debe imprimir en la consola la frase "Tu BMI es 20.061728395061728".
 */
