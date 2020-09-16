843a4ae142490d18924ba4c4e701fa15fcbdb162
Marlon Aurelio García Morales <clasesucatmarlon@gmail.com>
2020-09-16 14:50:31 +0000

    guess a number

--- Exercise_007.js
+++ Exercise_007.js
@@ -1,5 +1,5 @@
 /*
-Escribe un programa que piense un número de forma aleatoria del 1 al 10 y le pida al usuario que lo trate de adivinar. Si el número es correcto debe imprimir en la consola "Felicitaciones, ese era!", de lo contrario debe imprimir "Lo siento, intenta nuevamente!"
+Escribir un programa que piense un número de forma aleatoria del 1 al 10 y le pida al usuario que lo trate de adivinar. Si el número es correcto debe imprimir en la consola "Felicitaciones, ese era!", de lo contrario debe imprimir "Lo siento, intenta nuevamente!"
 */
 const r = Math.floor(Math.random() * 10) + 1
 number = prompt('Input a number between 1 and 10: ')
