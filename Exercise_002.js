7c4120b2e1ae1c57b0b1e0e4d1deb10ecb444d4d
Marlon Aurelio García Morales <clasesucatmarlon@gmail.com>
2020-09-16 14:57:51 +0000

    Print a greeting

--- Exercise_002.js
+++ Exercise_002.js
@@ -1,5 +1,5 @@
 /*
-Escribe un programa que le pregunte al usuario su nombre e imprima "Hola " seguido del nombre y un signo de exclamación.
+Escribir un programa que le pregunte al usuario su nombre e imprima "Hola " seguido del nombre y un signo de exclamación.
 
 Por ejemplo, si el usuario ingresa "Pedro" el programa debe imprimir en la consola "Hola Pedro!".
 */
